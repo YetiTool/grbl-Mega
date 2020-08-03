c610eeaa3920e68473586914da8b79899c14c8ea
grassbow <bk@grassbow.com>
2020-08-03 23:19:59 +0100

    v1.2.1 ZH2 update: change default settgins (acceleration and step invert mask)

--- grbl/ASMCNC.h
+++ grbl/ASMCNC.h
@@ -8,8 +8,8 @@
 #ifndef ASMCNC_h
 #define ASMCNC_h
 
-#define ASMCNC_VERSION			"1.2.0"
-#define ASMCNC_VERSION_BUILD	"20200804"
+#define ASMCNC_VERSION			"1.2.1"
+#define ASMCNC_VERSION_BUILD	"20200805"
 
 #define ASMCNC_STATUS_INVALID_STATEMENT	39 //ASM Error code 39 if 'A' is followed by unrecognised command
 
